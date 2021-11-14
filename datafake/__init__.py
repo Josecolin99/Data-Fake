b0eb8ac98e9603e79bbd9620359d4ea2e206ec0c
Colin <josecolin99@gmail.com>
2021-11-13 18:34:57 -0600

    Reorganización de la carpeta

---
+++ datafake/__init__.py
@@ -0,0 +1,12 @@
+from .datafake import DataFake
+
+df = DataFake
+
+RandomOneName = df.RandomOneName
+RandomTwoName = df.RandomTwoName
+RandomUSAName = df.RandomUSAName
+RandomName = df.RandomName
+CheckSex = df.CheckSex
+RandomSex = df.RandomSex
+RandomEmail = df.RandomEmail
+PersonalizedEmail = df.PersonalizedEmail
